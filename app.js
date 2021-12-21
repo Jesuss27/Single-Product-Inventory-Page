fc0f41e01bf37329afd351e3ffced4869b287ccb
Jesus <65581378+Jesuss27@users.noreply.github.com>
2021-12-20 16:09:45 -0800

    More Javascript!

--- app.js
+++ app.js
@@ -1,13 +1,24 @@
 const mobileHamburger = document.querySelector("#mobile");
 const mobileLinks = document.querySelector(".mobile-links")
+const heart = document.querySelector(".heart-icon")
+const desktopHamburger = document.querySelector("#desktop")
 
 
 
+heart.addEventListener("click",() => {
+    heart.classList.toggle("clicked")
+    console.log(heart)
+})
 
 mobileHamburger.addEventListener("click", () => {
     mobileHamburger.classList.toggle("active")
     mobileLinks.classList.toggle("is-active")
     console.log("click")
+})
+desktopHamburger.addEventListener("click", () => {
+    desktopHamburger.classList.toggle("desktop-active")
+    mobileLinks.classList.toggle("is-active")
+    console.log(desktopHamburger)
 })
 
 window.addEventListener("resize", () => {
